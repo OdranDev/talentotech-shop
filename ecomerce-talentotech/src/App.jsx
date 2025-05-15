22d51eb3502302a8a496c508bb100e2bde447309
odran21 <jleonardoguevaram@gmail.com>
2025-05-15 10:44:18 -0500

    Slider Horizontal Style

--- ecomerce-talentotech/src/App.jsx
+++ ecomerce-talentotech/src/App.jsx
@@ -1,30 +1,34 @@
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import ProductList from './components/ProductList'
-import Cart from './components/Cart'
-import Home from './components/Home'
-import DetalleProducto from './components/DetalleProducto'
-import RutaProtegida from './components/RutaProtegida'
-import Login from './components/Login'
-import ProductosPorCategoria from './components/ProductosPorCategoria'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import DetalleProducto from "./components/DetalleProducto";
+import RutaProtegida from "./components/RutaProtegida";
+import Login from "./components/Login";
+import ScrollToTop from "./components/ScrollToTop";
+// import ProductosPorCategoria from './components/ProductosPorCategoria'
 
 function App() {
   return (
-    <div className='App'>
-        <NavBar/>
+    <>
+      <ScrollToTop />
+      <div className="App">
+        <NavBar />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/cart' element={<RutaProtegida> <Cart/> </RutaProtegida>} />
-          <Route path='/productos' element={<ProductList/>} />
-          <Route path='/productos/:id' element={<DetalleProducto/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<RutaProtegida><Cart /></RutaProtegida>} />
+          <Route path="/productos" element={<ProductList />} />
+          <Route path="/productos/:id" element={<DetalleProducto />} />
           {/* <Route path="/categorias/:categoria" element={<ProductosPorCategoria />} /> */}
-          <Route path='/login' element={<Login />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-        <Footer/>
-    </div>
-  )
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
